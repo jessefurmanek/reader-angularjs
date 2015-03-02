e6b564b3adc75a6d13ed73b26a7f52d93165ad94
jessefurmanek <jesse.furmanek@gmail.com>
2015-03-02 12:26:29 -0600

    Add angular functinoality from exercises

--- js/controllers/ChapterController.js
+++ js/controllers/ChapterController.js
@@ -1,11 +1,16 @@
 app.controller('ChapterController', ['$scope', 'books', '$routeParams', function($scope, books, $routeParams) {
   books.success(function(data) {
     // Your code here
+  
+  books.success(function(data) {
+  $scope.book =  data[$routeParams.bookId];
+  $scope.chapter = $scope.book.chapters[$routeParams.chapterId];
+  });
 
 
 
     // If there no more chapters left, go back to the bookshelf view
-    if($routeParams.chapterId >= $scope.book.chapters.length - 1) {
+  if($routeParams.chapterId >= $scope.book.chapters.length - 1) {
       $scope.nextChapterIndex = "#";
     }
   });
